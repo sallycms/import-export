fddec3338513c986e3622f1d800ded04c7edbfaa
Zozi <zozi@webvariants.de>
2010-08-26 20:37:57 +0200

    use some exceptions

--- lib/sly/A1/Import/Files.php
+++ lib/sly/A1/Import/Files.php
@@ -1,14 +1,17 @@
 <?php
 /*
- * Copyright (C) 2009 REDAXO
+ * Copyright (c) 2010, webvariants GbR, http://www.webvariants.de
  *
- * This program is free software; you can redistribute it and/or modify it under
- * the terms of the GNU General Public License Version 2 as published by the
- * Free Software Foundation.
- */
+ * Diese Datei steht unter der MIT-Lizenz. Der Lizenztext befindet sich in der
+ * beiliegenden LICENSE Datei und unter:
+ *
+ * http://www.opensource.org/licenses/mit-license.php
+ * http://de.wikipedia.org/wiki/MIT-Lizenz
+*/
 
 /**
- * @package redaxo4
+ * Importer for Tar Archives
+ *
  */
 class sly_A1_Import_Files
 {
@@ -19,16 +22,12 @@ class sly_A1_Import_Files
 
 	public function import($filename)
 	{
-		$return['state'] = false;
-
-		if (!file_exists($filename)) {
-			$return['message'] = 'Datei nicht gefunden.<br />';
-			return $return;
+		if (empty($filename) || substr($filename, -7, 7) != '.tar.gz') {
+			throw new Exception(t('im_export_no_import_file_chosen'));
 		}
 
-		if (empty($filename) || substr($filename, -7, 7) != '.tar.gz') {
-			$return['message'] = t('im_export_no_import_file_chosen').'<br />';
-			return $return;
+		if (!file_exists($filename)) {
+			throw new Exception(t('im_export_selected_file_not_exists'));
 		}
 
 		$tar = new sly_A1_Archive_Tar($filename);
@@ -36,25 +35,15 @@ class sly_A1_Import_Files
 		chdir(SLY_BASE);
 
 		// Extensions auslösen
-
 		$tar = rex_register_extension_point('SLY_A1_BEFORE_FILE_IMPORT', $tar);
 
 		// Tar auspacken
-
 		if (!$tar->extract()) {
-			$msg = t('im_export_problem_when_extracting').'<br />';
-		}
-		else {
-			$msg = t('im_export_file_imported').'<br />';
+			throw new Exception(t('im_export_problem_when_extracting'));
 		}
 
 		// Extensions auslösen
-
 		$tar = rex_register_extension_point('SLY_A1_AFTER_FILE_IMPORT', $tar);
 		chdir('sally');
-
-		$return['state']   = true;
-		$return['message'] = $msg;
-		return $return;
 	}
 }
