6fbd03cbc2ad7c9725432bad56c11faf9a51c430
Zozi <zozi@webvariants.de>
2010-08-26 17:57:42 +0200

    refactor file structure + small fixes

--- config.inc.php
+++ config.inc.php
@@ -13,26 +13,4 @@ if (!defined('IS_SALLY_BACKEND')) return;
 
 $I18N->appendFile(SLY_INCLUDE_PATH.'/addons/import_export/lang/');
 
-// Autoloading initialisieren
-
-function _sly_a1_autoload($className)
-{
-	$class   = $className['subject'];
-	$classes = array(
-		'sly_A1_PEAR'            => 'class.pear.php',
-		'sly_A1_Archive_Tar'     => 'class.tar.php',
-		'sly_A1_Helper'          => 'class.helper.php',
-		'sly_A1_Import_Database' => 'class.import.database.php',
-		'sly_A1_Export_Database' => 'class.export.database.php',
-		'sly_A1_Import_Files'    => 'class.import.files.php',
-		'sly_A1_Export_Files'    => 'class.export.files.php'
-	);
-
-	if (isset($classes[$class])) {
-		require_once dirname(__FILE__).'/classes/'.$classes[$class];
-		return '';
-	}
-}
-
 sly_Loader::addLoadPath(dirname(__FILE__).DIRECTORY_SEPARATOR.'lib');
-rex_register_extension('__AUTOLOAD', '_sly_a1_autoload');
