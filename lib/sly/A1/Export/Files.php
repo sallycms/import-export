66fa3730518826c7bb2e7bd70171126d259e8bb6
Zozi <zozi@webvariants.de>
2011-01-20 17:29:04 +0100

    2010 -> 2011 + bugfix for windows

--- lib/sly/A1/Export/Files.php
+++ lib/sly/A1/Export/Files.php
@@ -1,18 +1,18 @@
 <?php
+
 /*
- * Copyright (c) 2010, webvariants GbR, http://www.webvariants.de
+ * Copyright (c) 2011, webvariants GbR, http://www.webvariants.de
  *
  * Diese Datei steht unter der MIT-Lizenz. Der Lizenztext befindet sich in der
  * beiliegenden LICENSE Datei und unter:
  *
  * http://www.opensource.org/licenses/mit-license.php
  * http://de.wikipedia.org/wiki/MIT-Lizenz
-*/
+ */
 
-class sly_A1_Export_Files
-{
-	public function export($filename, $files)
-	{
+class sly_A1_Export_Files {
+
+	public function export($filename, $files) {
 		// Archiv an einem temporären Ort erzeugen (Rekursion vermeiden)
 		$tmpFile = tempnam(sys_get_temp_dir(), 'sly');
 
@@ -20,21 +20,23 @@ class sly_A1_Export_Files
 		$archive = new ZipArchive();
 		$success = $archive->open($tmpFile, ZipArchive::OVERWRITE);
 
-		if($success === true) {
+		if ($success === true) {
 			chdir(SLY_BASE);
-			foreach($files as $file) {
-				if(is_dir($file)) {
+			foreach ($files as $file) {
+				if (is_dir($file)) {
 					$dir = new sly_Util_Directory($file, false);
-					foreach($dir->listRecursive(true, false) as $dirfile) {
-						$success = $archive->addFile($file.DIRECTORY_SEPARATOR.$dirfile, $file.DIRECTORY_SEPARATOR.$dirfile);
-						if($success !== true) break;
+					foreach ($dir->listRecursive(true, false) as $dirfile) {
+						$success = $archive->addFromString(str_replace(DIRECTORY_SEPARATOR, '/', $file . DIRECTORY_SEPARATOR . $dirfile), file_get_contents($file . DIRECTORY_SEPARATOR . $dirfile));
+						if ($success !== true)
+							break;
 					}
 				} else {
-					$success = $archive->addFile($file, $file);
-					if($success !== true) break;
+					$success = $archive->addFromString(str_replace(DIRECTORY_SEPARATOR, '/', $file), file_get_contents($file));
+					if ($success !== true)
+						break;
 				}
 			}
-			if($success !== true) {
+			if ($success !== true) {
 				throw new sly_Exception('im_export_failed_to_create_archive', $ok);
 			}
 			$archive->close();
@@ -42,7 +44,8 @@ class sly_A1_Export_Files
 
 		chdir('sally');
 		rename($tmpFile, $filename);
-		chmod($filename, 0774);
+		chmod($filename, 0777);
 		return $success;
 	}
+
 }
