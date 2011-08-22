041614acc50a3c54a0a373a44712ddd9576fb03e
Christoph <christoph@webvariants.de>
2011-08-22 12:03:58 +0200

    removed unused arguments

--- lib/sly/A1/Export/Files/ZipArchive.php
+++ lib/sly/A1/Export/Files/ZipArchive.php
@@ -11,7 +11,7 @@
 class sly_A1_Export_Files_ZipArchive extends sly_A1_Export_Files {
 	public function export($filename, $files, $addons) {
 		$tmpFile   = $this->getTempFileName();
-		$archive   = new ZipArchive($archive, $baseDir, $file);
+		$archive   = new ZipArchive();
 		$success   = $archive->open($tmpFile, ZipArchive::OVERWRITE);
 		$isWindows = DIRECTORY_SEPARATOR === '\\';
 
